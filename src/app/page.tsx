8845d0f945b9cf94d601dbf199685af8e17ce128
Cassiano <Cassianocosta12357@gmail.com>
2026-01-19 18:19:19 -0300

    add: Whatsapp button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 import { Metadata } from "next";
 
@@ -13,11 +14,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <section className="bg-[url('/hero.png')] bg-cover h-[calc(100vh-192px)] bg-center relative before:absolute before:bg-[rgba(0,0,0,0.8)] before:inset-0 before:z-0 flex justify-center items-center mx-30 my-5 rounded-4xl before:rounded-4xl">
-        <div className="z-2">
-          <h1 className="text-white">HERO</h1>
-        </div>
-      </section>
+      <Hero />
       <Footer />
     </>
   );
---
+++ src/components/WhatsButton/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function WhatsButton() {
+    const NumeroWhats = "https://api.whatsapp.com/send?phone=5551998893255"
+
+    return (
+        <div className="flex fixed justify-center items-center bottom-8 right-8 z-10 cursor-pointer">
+            <a href={NumeroWhats} target="_blank">
+                <Image className="" src="/whatsapp.png" alt="WhatsApp" width={64} height={64} />
+            </a>
+        </div>
+    )
+}
