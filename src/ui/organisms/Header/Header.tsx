34dbe169a54660e9312071270012b059251c25fd
Cassiano <Cassianocosta12357@gmail.com>
2026-02-01 22:59:20 -0300

    update

--- src/ui/organisms/Header/Header.tsx
+++ src/ui/organisms/Header/Header.tsx
@@ -22,16 +22,16 @@ export default function Header() {
 
 
     return (
-        <header className="flex items-center h-24 w-full bg-white sticky top-0 left-0 z-10 shadow-md">
+        <header className={logoVisible ? 'opacity-100 transition-opacity flex items-center h-24 w-full bg-white sticky top-0 left-0 z-10 shadow-md' : 'opacity-0'}>
             <div className="flex justify-between items-center w-full px-8 md:px-0 max-w-7xl mx-auto">
-                <div className={logoVisible ? 'opacity-100 transition-opacity duration-500' : 'opacity-0 transition-opacity duration-500'}>
+                <div>
                     <Logo width={240} height={49} />
                 </div>
                 <div className="flex items-center">
                     <div className="block md:hidden">
                         <SidebarTrigger size="icon-sm" />
                     </div>
-                    <div className={logoVisible ? 'opacity-100 transition-opacity duration-500' : 'opacity-0 transition-opacity duration-500'}>
+                    <div>
                         <div className="hidden md:flex">
                             <NavHeader />
                         </div>
