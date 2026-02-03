db01ceef789ff928f94728cef2a00b0c266d59c7
Cassiano <Cassianocosta12357@gmail.com>
2026-02-03 20:12:59 -0300

    update

--- src/ui/molecules/WhatsButton/WhatsButton.tsx
+++ src/ui/molecules/WhatsButton/WhatsButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 
 export default function WhatsButton() {
-  const NumeroWhats = "https://api.whatsapp.com/send?phone=5551997240438";
+  const NumeroWhats = "https://api.whatsapp.com/send?phone=5551997250438";
 
   return (
     <div className="flex fixed justify-center items-center bottom-8 right-8 z-10 cursor-pointer transition-transform hover:scale-110">
